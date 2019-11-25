b5ff72ee3e291c91c752ced2ac40063cd85b9f4d
maxipopuliz <maxipopuliz@gmail.com>
2019-11-25 16:20:10 -0500

    sing up and log in works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-const { GraphQLServer } = require('graphql-yoga')
+const Mutation = require("./resolvers/Mutation");
+const { prisma } = require("./generated/prisma-client");
+const { GraphQLServer } = require("graphql-yoga");
+
+const resolvers = {
+  Query: { info: () => "Oasis test Query" },
+  Mutation
+};
 
 const server = new GraphQLServer({
-  //TODO
-})
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
+});
 
-server.start(() => console.log('Server is running on somewhere'))
+server.start(() => console.log("Server is running on 4000"));
