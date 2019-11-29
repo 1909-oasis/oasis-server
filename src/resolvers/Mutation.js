533ae7a57f303fe1b219b2d2e9d3ac4e68840423
Dan Erbland <danerbland@gmail.com>
2019-11-29 15:42:56 -0500

    added createQueue resolver and updated schema

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const {queue} = require('./User')
 const { APP_SECRET, getUserId } = require("../utils");
 
 async function signup(parent, args, context, info) {
@@ -46,10 +47,36 @@ async function swipe(parent, args, context, info) {
 }
 
 async function updateQueue(parent, args, context, info) {
+  console.log('in updateQueue')
   const userId = getUserId(context);
+
   const updatedUser = await context.prisma.updateUser({
     data: {
       queue: args.cocktailArr,
+    },
+    where: {
+      id: userId,
+    },
+  });
+  return updatedUser;
+}
+
+//will always replace the entire queue with a new one.
+//takes an array of cocktail Ids as an argument (cocktailIds)
+async function createQueue(parent, args, context, info) {
+  console.log('in addToQueue')
+  const userId = getUserId(context)
+  const cocktailIds = args.cocktailIds
+
+  //GraphQL Shenanigans:  the 'queue' field in updateUser() expects a CocktailUpdateManyInput object which has a 'connect' key.  This 'connect' key expects an array of [CocktailWhereUniqueInput!] objects, each of which can be formatted as {id: cocktailId} to connect with the right cocktail in our Cocktail table.  Soo we create an array of these CocktailWhereUniqueInput objects here!!!
+  let connectArray = cocktailIds.map((cocktailId) => {
+    return {id: cocktailId}
+  })
+
+  //now we update the user with the array of {id: cocktailId} objects.
+  const updatedUser = await context.prisma.updateUser({
+    data: {
+      queue: {connect: connectArray},
     },
     where: {
       id: userId,
@@ -63,4 +90,5 @@ module.exports = {
   login,
   swipe,
   updateQueue,
+  createQueue
 };
