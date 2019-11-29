56769c56e5d44e9c07d4872d1dbe7793f93d328e
Samantha Lastig <slastig13@gmail.com>
2019-11-29 10:21:27 -0500

    starterPack query is working

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,17 +1,29 @@
-const { APP_SECRET, getUserId } = require("../utils");
+const {APP_SECRET, getUserId} = require('../utils')
 
-function info (){
-  return "Oasis test Query"
+function info() {
+  return 'Oasis test Query'
 }
 
-async function me (parent, args, context, info){
+async function me(parent, args, context, info) {
   console.log('we are in meeeeee')
   return await context.prisma.user({
     id: getUserId(context)
   })
 }
 
+async function cocktailStarter(parent, args, context, info) {
+  console.log('we are in starter pack')
+  const pack = await context.prisma.cocktails({
+    where: {
+      starterPack: args.starterPack
+    }
+  })
+  console.log(`this is pack`, pack)
+  return pack
+}
+
 module.exports = {
   info,
-  me
+  me,
+  cocktailStarter
 }
