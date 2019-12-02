ada6b2bdfc1fb6fec9f6426563730f882f564642
Samantha Lastig <slastig13@gmail.com>
2019-12-02 15:44:54 -0500

    updated swipe mutuation

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,49 +1,44 @@
-const {APP_SECRET, getUserId} = require('../utils')
+const { APP_SECRET, getUserId } = require("../utils");
 
 function info() {
-  return 'Oasis test Query'
+  return "Oasis test Query";
 }
 
-async function me (parent, args, context, info){
-  console.log('we are in meeeeee')
+async function me(parent, args, context, info) {
   return await context.prisma.user({
-    id: getUserId(context)
-  })
+    id: getUserId(context),
+  });
 }
 
-async function dan (parent, args, context, info){
-  console.log('we are in dan')
+async function dan(parent, args, context, info) {
   return await context.prisma.user({
-    id: "ck3gews83000e0740izc0ska5"
-  })
+    id: "ck3gews83000e0740izc0ska5",
+  });
 }
 
 // returns array of starterpack cocktails if starterpack is set to true in query.
 async function cocktailStarter(parent, args, context, info) {
-  console.log('we are in starter pack')
   const pack = await context.prisma.cocktails({
     where: {
-      starterPack: args.starterPack
-    }
-  })
-  console.log(`this is pack`, pack)
-  return pack
+      starterPack: args.starterPack,
+    },
+  });
+
+  return pack;
 }
 
-async function recommendationList (parent, args, context, info) {
-  console.log('in recommendationList')
+async function recommendationList(parent, args, context, info) {
   const list = await context.prisma.userCocktails({
     where: {
       user: { id: getUserId(context) },
-      recommended: true
-    }
-  })
-  return list
+      recommended: true,
+    },
+  });
+  return list;
 }
 
 //returns 20 random unrated cocktails (or however many are left if less than 20)
-async function unratedCocktails (parent, args, context, info){
-
+async function unratedCocktails(parent, args, context, info) {
   const fragment = `
   fragment UserCocktailWithCocktail on UserCocktail {
     id
@@ -53,30 +48,38 @@ async function unratedCocktails (parent, args, context, info){
       name
     }
   }
-`
+`;
 
-  const ratedUserCocktails = await context.prisma.userCocktails({
+  const ratedUserCocktails = await context.prisma
+    .userCocktails({
+      where: {
+        user: { id: getUserId(context) },
+      },
+    })
+    .$fragment(fragment);
+  cocktailIdsToExclude = ratedUserCocktails.map(
+    element => element.cocktail.id
+  );
+
+  const unratedCocktails = await context.prisma.cocktails({
     where: {
-      user: { id: getUserId(context)}
-    }
-  }).$fragment(fragment)
-  cocktailIdsToExclude = ratedUserCocktails.map((element)=>element.cocktail.id)
+      NOT: [
+        {
+          id_in: cocktailIdsToExclude,
+        },
+      ],
+    },
+  });
 
-  const unratedCocktails =  await context.prisma.cocktails({
-    where:
-    {NOT: [{
-      id_in: cocktailIdsToExclude
-    }]}
-  })
-
-  returnArr = []
-  for(let i = 0; i < 20 && i < unratedCocktails.length; i++){
-    const randomIndex = Math.floor(Math.random() * unratedCocktails.length)
-    returnArr.push(unratedCocktails[randomIndex])
+  returnArr = [];
+  for (let i = 0; i < 20 && i < unratedCocktails.length; i++) {
+    const randomIndex = Math.floor(
+      Math.random() * unratedCocktails.length
+    );
+    returnArr.push(unratedCocktails[randomIndex]);
   }
 
-
-  return returnArr
+  return returnArr;
 }
 
 module.exports = {
@@ -85,5 +88,5 @@ module.exports = {
   dan,
   cocktailStarter,
   recommendationList,
-  unratedCocktails
-}
+  unratedCocktails,
+};
